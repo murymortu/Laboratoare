05c2821a50438bca2aed2270d56210b2a07326c8
Alexandru Muresan <murymortu@yahoo.com>
2013-03-21 21:25:21 +0200

    al-2-lea commit

---
+++ src/ClasaNoua.java
@@ -0,0 +1,10 @@
+/**
+ * User: Alexandru
+ * Date: 3/21/13
+ * Time: 9:21 PM
+ */
+public class ClasaNoua {
+    private int a = 20;
+    public short b = 5;
+    private String string2 = "acesta este alt string Git git git etc";
+}
